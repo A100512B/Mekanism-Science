7ac092de0f85eb5abbd2f28e01aefe1c651538f4
A100512B <mikuLover_a1@outlook.com>
2026-05-09 23:12:23 +0800

    register sounds

--- src/main/java/com/fxd927/mekanismscience/common/registries/MSSounds.java
+++ src/main/java/com/fxd927/mekanismscience/common/registries/MSSounds.java
@@ -11,7 +11,9 @@ public class MSSounds {
 
     public static final SoundEventDeferredRegister SOUND_EVENTS = new SoundEventDeferredRegister(MekanismScience.MODID);
 
+    public static final SoundEventRegistryObject<SoundEvent> PRESSURIZED_POLYMERIZER = SOUND_EVENTS.register("tile.machine.pressurized_polymerizer");
     public static final SoundEventRegistryObject<SoundEvent> EXTRACTING_PLANT = SOUND_EVENTS.register("tile.machine.extracting_plant");
     public static final SoundEventRegistryObject<SoundEvent> ANTI_EXTRACTING_PLANT = SOUND_EVENTS.register("tile.machine.anti_extracting_plant");
     public static final SoundEventRegistryObject<SoundEvent> METAL_ELECTROLYSIS_CHAMBER = SOUND_EVENTS.register("tile.machine.metal_electrolysis_chamber");
+    public static final SoundEventRegistryObject<SoundEvent> ACID_LEACHER = SOUND_EVENTS.register("tile.machine.acid_leacher");
 }
