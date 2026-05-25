056a85aad6a019e5484664aeb4b6507a2de69fab
A100512B <mikuLover_a1@outlook.com>
2026-05-25 21:31:54 +0800

    port adsorption separator and irradiator back here

--- src/main/java/com/fxd927/mekanismscience/common/registries/MSContainerTypes.java
+++ src/main/java/com/fxd927/mekanismscience/common/registries/MSContainerTypes.java
@@ -20,6 +20,8 @@ public class MSContainerTypes {
     public static final ContainerTypeRegistryObject<MekanismTileContainer<TileEntityMetalElectrolysisChamberCasing>> METAL_ELECTROLYSIS_CHAMBER = CONTAINER_TYPES.register("metal_electrolysis_chamber", TileEntityMetalElectrolysisChamberCasing.class);
     public static final ContainerTypeRegistryObject<MekanismTileContainer<TileEntityAcidLeacher>> ACID_LEACHER = CONTAINER_TYPES.register("acid_leacher", TileEntityAcidLeacher.class);
     public static final ContainerTypeRegistryObject<MekanismTileContainer<TileEntityAirCompressor>> AIR_COMPRESSOR = CONTAINER_TYPES.register("air_compressor", TileEntityAirCompressor.class);
+    public static final ContainerTypeRegistryObject<MekanismTileContainer<TileEntityAdsorptionSeparator>> ADSORPTION_SEPARATOR = CONTAINER_TYPES.register(MSBlocks.ADSORPTION_SEPARATOR, TileEntityAdsorptionSeparator.class);
+    public static final ContainerTypeRegistryObject<MekanismTileContainer<TileEntityIrradiator>> IRRADIATOR = CONTAINER_TYPES.register(MSBlocks.IRRADIATOR, TileEntityIrradiator.class);
 
     private MSContainerTypes(){
     }
